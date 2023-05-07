6d27a5a1059a74a847815852b41dc3271c183ffd
Toki <55911995+tokio-k@users.noreply.github.com>
2023-05-07 21:00:58 +0900

    条件分岐バグ修正

--- contracts/NFTExchange.sol
+++ contracts/NFTExchange.sol
@@ -2,11 +2,6 @@
 pragma solidity ^0.8.9;
 
 contract NFTExchange {
-
-    event OrderCreated(address indexed sender, address indexed senderNFTContractAddress, uint256 senderNFTTokenId, address indexed receiverNFTContractAddress, uint256 receiverNFTTokenId);
-    event OrderApproved(address indexed sender, address indexed approver);
-
-
     mapping(address => Order) private myOrder;
 
     struct Order {
@@ -20,24 +15,30 @@ contract NFTExchange {
         if(IERC721(_senderNFTContractAddress).getApproved(_senderNFTTokenId) != address(this)) revert("NFT you hold is not approve");
 
         myOrder[msg.sender] =  Order(_senderNFTContractAddress, _senderNFTTokenId, _receiverNFTContractAddress, _receiverNFTTokenId);
-        emit OrderCreated(msg.sender, _senderNFTContractAddress, _senderNFTTokenId, _receiverNFTContractAddress, _receiverNFTTokenId);
     }
 
-    function approveOrder(address orderSender) public {
-        if(myOrder[orderSender].senderNFTContractAddress == address(0)) revert("Order Not Found");
-
+    function approveOrder(address _senderNFTContractAddress, uint256 _senderNFTTokenId, address _receiverNFTContractAddress, uint256 _receiverNFTTokenId,address orderSender) public {
         Order memory order = myOrder[orderSender];
+
+        if(order.senderNFTContractAddress == address(0)) revert("Order Not Found");
+        if(order.senderNFTContractAddress != _senderNFTContractAddress || order.senderNFTTokenId != _senderNFTTokenId || order.receiverNFTContractAddress != _receiverNFTContractAddress || order.receiverNFTTokenId != _receiverNFTTokenId){
+            revert("Order has been Changed");
+        }
+
         IERC721 orderSenderNFT = IERC721(order.senderNFTContractAddress);
         IERC721 orderReceiverNFT = IERC721(order.receiverNFTContractAddress);
 
-        if(orderSenderNFT.ownerOf(order.senderNFTTokenId) == orderSender) revert("Order Registrant does not have this NFT");
-        if(orderReceiverNFT.ownerOf(order.receiverNFTTokenId) == msg.sender) revert("You do not have this NFT");
+        if(orderSenderNFT.ownerOf(order.senderNFTTokenId) != orderSender) revert("Order Registrant does not have this NFT");
+        if(orderReceiverNFT.ownerOf(order.receiverNFTTokenId) != msg.sender) revert("You do not have this NFT");
 
         orderSenderNFT.transferFrom(orderSender, msg.sender, order.senderNFTTokenId);
         orderReceiverNFT.transferFrom(msg.sender, orderSender, order.receiverNFTTokenId);
 
         delete myOrder[orderSender];
-        emit OrderApproved(orderSender, msg.sender);
+    }
+
+    function closeOrder() public {
+        delete myOrder[msg.sender];
     }
 
     function getOrder(address orderSender) public view returns(Order memory) {
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const ACCOUNT_PRIVATE_KEY_DEV = process.env.ACCOUNT_PRIVATE_KEY_DEV ?? "";
 const ACCOUNT_PRIVATE_KEY_MAIN = process.env.ACCOUNT_PRIVATE_KEY_MAIN ?? "";
 
 //dev
@@ -16,20 +17,27 @@ const ALCHEMY_API_KEY_MAIN = process.env.ALCHEMY_API_KEY_MAIN;
 const ALCHEMY_API_KEY_MUMBAI = process.env.ALCHEMY_API_KEY_MUMBAI;
 const ACCOUNT_PRIVATE_KEY_MUMBAI = process.env.ACCOUNT_PRIVATE_KEY_MUMBAI ?? "";
 
+//sepolia
+const ALCHEMY_API_KEY_SEPOLIA = process.env.ALCHEMY_API_KEY_SEPOLIA;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY_GOERIL}`,
-      accounts: [ACCOUNT_PRIVATE_KEY_MAIN],
-    },
+    // goerli: {
+    //   url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY_GOERIL}`,
+    //   accounts: [ACCOUNT_PRIVATE_KEY_DEV],
+    // },
+    // sepolia: {
+    //   url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY_SEPOLIA}`,
+    //   accounts: [ACCOUNT_PRIVATE_KEY_MAIN],
+    // },
     // mainnet: {
     //   url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY_MAIN}`,
-    //   accounts: [ACCOUNT_PRIVATE_KEY_MAIN],
+    //   accounts: [ACCOUNT_PRIVATE_KEY_DEV],
     // },
     // mumbai: {
     //   url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY_MUMBAI}`,
-    //   accounts: [ACCOUNT_PRIVATE_KEY_MUMBAI],
+    //   accounts: [ACCOUNT_PRIVATE_KEY_DEV],
     // },
   },
   etherscan: {
